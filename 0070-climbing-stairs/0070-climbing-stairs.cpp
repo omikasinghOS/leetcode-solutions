5dc9072ab58ae1a72004a5cf1a473dee59965ad0
omikasinghOS <109844423+omikasinghOS@users.noreply.github.com>
2026-06-05 11:41:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int prev2 = 1, prev1 = 2;
+        int prev1 = 2 ,prev2 = 1 ;
         for (int i =3; i <= n; i++) {
             int cur= prev1 + prev2;
             prev2 =prev1;
