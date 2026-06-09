b41eabb00767e9b6ebdd3b47d2d62abd69e8167c
omikasinghOS <109844423+omikasinghOS@users.noreply.github.com>
2026-06-09 16:18:34 +0530

    Time: 0 ms (100%), Space: 10.8 MB (38.14%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> subset;
+
+    void backtrack(vector<int>& nums, int start) {
+        result.push_back(subset);
+
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            subset.push_back(nums[i]);
+            backtrack(nums, i + 1);
+            subset.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        backtrack(nums, 0);
+        return result;
+    }
+};
